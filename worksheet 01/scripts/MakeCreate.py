a09d731d78700a95e2fc41257d21bb2c310dd3ec
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2015-10-29 19:49:43 +0100

    Rough draft of script to create Makefile with flag combinations

--- worksheet 01/scripts/MakeCreate.py
+++ worksheet 01/scripts/MakeCreate.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/pyton
 
 import itertools
@@ -7,23 +6,32 @@ import sys
 import time
 import fileinput
 
-
-file = open('testfile','w')
+f_handle = open('testfile','w')
 numbers = ["1","2","3","4","5"]
 intel = ["-march = native","-fomit-fram-pointer","-floop-block","floop-interchange","floop-strip-mine","funroll-loops","-flto"]
-file = open('testfile','w')
+f_handle = open('testfile','w')
 #fileinput.filename()
+
 for i in range(1,len(intel)):
+
 	combination = list(itertools.combinations(intel,i)) 
-#	print([x for x in itertools.combinations(numbers, i)])
 	flag_string= map(' '.join,combination)
-#	print(combination[0])
-	for j in range(0,len(intel)):
-#		print(flag_string[j])
-#		for line in file
-#			if line.contains('foo'):
-				
-		file.write(flag_string[j])
-		file.write("\n")
-	#TODO input flags into Makefile at deesired position with desired syntax
+	
+	flag_searchline = '#flag_list'
+	target_searchline = '#target_list'
+
+	lines = f_handle.readlines()
+	flags_index = lines.index(flag_searchline)
+	targets_index =  lines.index(target_searchline)
+
+	for j in range(0,len(flag_string)):
+		flag_lines += "CXXFLAGS_i = -O3 -I. -w " + flag_string[j] + "\n"
+
+	for k in range(0,len(flag_string)):
+		target_lines += "name_" + k + ": lulesh.h $(CXX) -c $(CXXFLAGS_" + k + " -o $@  $< " + "\n" 
+
+	for line in f_handle
+		lines.insert(flags_index, flag_lines)
+		lines.insert(targets_index, target_lines)
+
 file.close()
