ee9ebe43dda70bc08b48e8ca2474dccb88f6a3bb
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2015-11-03 14:59:17 +0100

    Document to suggest pragmas in CalcHourglassControlForElems

---
+++ worksheet 01/Pragma_suggestions.cc
@@ -0,0 +1,65 @@
+static inline
+void CalcHourglassControlForElems(Domain& domain,
+                                  Real_t determ[], Real_t hgcoef)
+{
+   Index_t numElem = domain.numElem() ;
+   Index_t numElem8 = numElem * 8 ;
+   Real_t *dvdx = Allocate<Real_t>(numElem8) ;
+   Real_t *dvdy = Allocate<Real_t>(numElem8) ;
+   Real_t *dvdz = Allocate<Real_t>(numElem8) ;
+   Real_t *x8n  = Allocate<Real_t>(numElem8) ;
+   Real_t *y8n  = Allocate<Real_t>(numElem8) ;
+   Real_t *z8n  = Allocate<Real_t>(numElem8) ;
+
+   /* start loop over elements */
+#pragma omp parallel for firstprivate(numElem)
+   for (Index_t i=0 ; i<numElem ; ++i){
+      Real_t  x1[8],  y1[8],  z1[8] ;
+      Real_t pfx[8], pfy[8], pfz[8] ;
+
+      Index_t* elemToNode = domain.nodelist(i);
+      CollectDomainNodesToElemNodes(domain, elemToNode, x1, y1, z1);
+
+      CalcElemVolumeDerivative(pfx, pfy, pfz, x1, y1, z1);
+
+      /* load into temporary storage for FB Hour Glass control */
+
+      #pragma 
+      for(Index_t ii=0;ii<8;++ii){
+         Index_t jj=8*i+ii;
+
+         dvdx[jj] = pfx[ii];
+         dvdy[jj] = pfy[ii];
+         dvdz[jj] = pfz[ii];
+         x8n[jj]  = x1[ii];
+         y8n[jj]  = y1[ii];
+         z8n[jj]  = z1[ii];
+      }
+
+      determ[i] = domain.volo(i) * domain.v(i);
+
+      /* Do a check for negative volumes */
+      if ( domain.v(i) <= Real_t(0.0) ) {
+#if USE_MPI         
+         MPI_Abort(MPI_COMM_WORLD, VolumeError) ;
+#else
+         exit(VolumeError);
+#endif
+      }
+   }
+
+   if ( hgcoef > Real_t(0.) ) {
+      CalcFBHourglassForceForElems( domain,
+                                    determ, x8n, y8n, z8n, dvdx, dvdy, dvdz,
+                                    hgcoef, numElem, domain.numNode()) ;
+   }
+
+   Release(&z8n) ;
+   Release(&y8n) ;
+   Release(&x8n) ;
+   Release(&dvdz) ;
+   Release(&dvdy) ;
+   Release(&dvdx) ;
+
+   return ;
+}
