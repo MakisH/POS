bfbf83bf27bb8ed2dc0cec60433c610ec6359829
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2015-11-04 12:07:28 +0100

    Changed suggestions to just one, as the worksheet suggests.

--- worksheet 01/Pragma_suggestions.cc
+++ worksheet 01/Pragma_suggestions.cc
@@ -65,74 +65,74 @@ void CalcHourglassControlForElems(Domain& domain,
    return ;
 }
 
-//static inline
-void CalcKinematicsForElems( Domain &domain, Real_t *vnew, 
-                             Real_t deltaTime, Index_t numElem )
-{
+// //static inline
+// void CalcKinematicsForElems( Domain &domain, Real_t *vnew, 
+//                              Real_t deltaTime, Index_t numElem )
+// {
 
-  // loop over all elements
-#pragma omp parallel for firstprivate(numElem, deltaTime)
-  for( Index_t k=0 ; k<numElem ; ++k )
-  {
-    Real_t B[3][8] ; /** shape function derivatives */
-    Real_t D[6] ;
-    Real_t x_local[8] ;
-    Real_t y_local[8] ;
-    Real_t z_local[8] ;
-    Real_t xd_local[8] ;
-    Real_t yd_local[8] ;
-    Real_t zd_local[8] ;
-    Real_t detJ = Real_t(0.0) ;
+//   // loop over all elements
+// #pragma omp parallel for firstprivate(numElem, deltaTime)
+//   for( Index_t k=0 ; k<numElem ; ++k )
+//   {
+//     Real_t B[3][8] ; /** shape function derivatives */
+//     Real_t D[6] ;
+//     Real_t x_local[8] ;
+//     Real_t y_local[8] ;
+//     Real_t z_local[8] ;
+//     Real_t xd_local[8] ;
+//     Real_t yd_local[8] ;
+//     Real_t zd_local[8] ;
+//     Real_t detJ = Real_t(0.0) ;
 
-    Real_t volume ;
-    Real_t relativeVolume ;
-    const Index_t* const elemToNode = domain.nodelist(k) ;
+//     Real_t volume ;
+//     Real_t relativeVolume ;
+//     const Index_t* const elemToNode = domain.nodelist(k) ;
 
-    // get nodal coordinates from global arrays and copy into local arrays.
-    CollectDomainNodesToElemNodes(domain, elemToNode, x_local, y_local, z_local);
+//     // get nodal coordinates from global arrays and copy into local arrays.
+//     CollectDomainNodesToElemNodes(domain, elemToNode, x_local, y_local, z_local);
 
-    // volume calculations
-    volume = CalcElemVolume(x_local, y_local, z_local );
-    relativeVolume = volume / domain.volo(k) ;
-    vnew[k] = relativeVolume ;
-    domain.delv(k) = relativeVolume - domain.v(k) ;
+//     // volume calculations
+//     volume = CalcElemVolume(x_local, y_local, z_local );
+//     relativeVolume = volume / domain.volo(k) ;
+//     vnew[k] = relativeVolume ;
+//     domain.delv(k) = relativeVolume - domain.v(k) ;
 
-    // set characteristic length
-    domain.arealg(k) = CalcElemCharacteristicLength(x_local, y_local, z_local,
-                                             volume);
+//     // set characteristic length
+//     domain.arealg(k) = CalcElemCharacteristicLength(x_local, y_local, z_local,
+//                                              volume);
 
-    // get nodal velocities from global array and copy into local arrays.
+//     // get nodal velocities from global array and copy into local arrays.
 
-    #pragma vector
-    for( Index_t lnode=0 ; lnode<8 ; ++lnode )
-    {
-      Index_t gnode = elemToNode[lnode];
-      xd_local[lnode] = domain.xd(gnode);
-      yd_local[lnode] = domain.yd(gnode);
-      zd_local[lnode] = domain.zd(gnode);
-    }
+//     #pragma vector
+//     for( Index_t lnode=0 ; lnode<8 ; ++lnode )
+//     {
+//       Index_t gnode = elemToNode[lnode];
+//       xd_local[lnode] = domain.xd(gnode);
+//       yd_local[lnode] = domain.yd(gnode);
+//       zd_local[lnode] = domain.zd(gnode);
+//     }
 
-    Real_t dt2 = Real_t(0.5) * deltaTime;
+//     Real_t dt2 = Real_t(0.5) * deltaTime;
 
-    #pragma vector
-    for ( Index_t j=0 ; j<8 ; ++j )
-    {
-       x_local[j] -= dt2 * xd_local[j];
-       y_local[j] -= dt2 * yd_local[j];
-       z_local[j] -= dt2 * zd_local[j];
-    }
+//     #pragma vector
+//     for ( Index_t j=0 ; j<8 ; ++j )
+//     {
+//        x_local[j] -= dt2 * xd_local[j];
+//        y_local[j] -= dt2 * yd_local[j];
+//        z_local[j] -= dt2 * zd_local[j];
+//     }
 
-    CalcElemShapeFunctionDerivatives( x_local, y_local, z_local,
-                                      B, &detJ );
+//     CalcElemShapeFunctionDerivatives( x_local, y_local, z_local,
+//                                       B, &detJ );
 
-    CalcElemVelocityGradient( xd_local, yd_local, zd_local,
-                               B, detJ, D );
+//     CalcElemVelocityGradient( xd_local, yd_local, zd_local,
+//                                B, detJ, D );
 
-    // put velocity gradient quantities into their global arrays.
-    domain.dxx(k) = D[0];
-    domain.dyy(k) = D[1];
-    domain.dzz(k) = D[2];
-  }
-}
+//     // put velocity gradient quantities into their global arrays.
+//     domain.dxx(k) = D[0];
+//     domain.dyy(k) = D[1];
+//     domain.dzz(k) = D[2];
+//   }
+// }
 
 
