ac6e5723387933717069f618967d4ad0062c0139
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2015-11-13 12:45:49 +0100

    script

--- worksheet 01/scripts/FlagTesting.py
+++ worksheet 01/scripts/FlagTesting.py
@@ -7,13 +7,6 @@ import time
 import fileinput
 import subprocess
 
-
-def find(l, s):
-    for i in range(len(l)):
-        if l[i].find(s)!=-1:
-            return i
-    return None # Or -1
-
 test_all = 1
 intel = ["-march=native", "-xHost", "-unroll", "-ipo"]
 gcc = ["-march=native","-fomit-frame-pointer","-floop-block","-floop-interchange","-floop-strip-mine","-funroll-loops","-flto"]
@@ -22,7 +15,7 @@ script = "seq.ll"
 
 #fileinput.filename()
 
-if (test_all == 1)
+#if (test_all == 1)
 	flag_string = []
 	for i in range(0, len(intel)-1): 
 		combination = list(itertools.combinations(intel,i)) 
@@ -30,7 +23,5 @@ if (test_all == 1)
 	for j in range(0, len(flag_string)-1):
 		os.environ['FLAG_COMBINATION'] = flag_string[j]
 		subprocess.call(["bash" + script])
-else
-	
-
+#else
 
