2802c2c5ddc472ef3f1292f01f34d48ca52b71b9
MakisH <makishourdakis@gmail.com>
2015-11-13 15:43:16 +0100

    Cleaned the dataParase.py a bit.

--- worksheet 01/scripts/dataParse.py
+++ worksheet 01/scripts/dataParse.py
@@ -4,38 +4,43 @@ import fileinput
 import os
 import csv
 
+# Set-up of structures
 Data = [("filename", "Elapsed time", "Grind time", "FOM")]
-tup = [0]*4
-for i in os.listdir(os.getcwd()):
-    if i.endswith(".txt"):
-	print("opening file... \n" + i)
-	file = open(i , 'r')
+info_buffer = [0]*4
+
+# Iterate through all the files in the current directory and
+# grab the relevant information from all the *.out files.
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".out"):
+	print("opening file... \n" + filename)
+	file = open(filename , 'r')
 	row = file.readlines()
-	tup[0] = str(i)
+	info_buffer[0] = str(filename)
 	for line in row:
+        # Grab the Elapsed time
 	    if line.find("Elapsed time") > -1:
 		info = line.split()
-	        title = ' '.join([i, info[0] , info[1]])
 		time = info[3]
-		tup[1] = time
+		info_buffer[1] = time
+        # Grab the Grind time
 	    if line.find("Grind time") > -1:
 		info = line.split()
 		Gtime = info[4]
-		tup[2] = Gtime
+		info_buffer[2] = Gtime
+        # Grab the FOM
 	    if line.find("FOM") > -1:
 		info = line.split()
 		FOM = info [2]
-		tup[3] = FOM
-#	print(" ".join([title,  "=", time, Gtime, FOM]))
-	Data.append(tuple(tup))
+		info_buffer[3] = FOM
+    # Make the info_buffer a tuple
+	Data.append(tuple(info_buffer))
+    # Close the file
 	file.close()
-#TODO output data to .csv
-outFile = "output.ods"
+
+# output the data to .csv
+outFile = "output.csv"
 print("writing output to: " + outFile + "\n")
 with open(outFile, "w") as out:
     csv_out=csv.writer(out)
-#    csv_out.writerow(['name','num'])
     for row in Data:
         csv_out.writerow(row)
-
-#print(Data)
